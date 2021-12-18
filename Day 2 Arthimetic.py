95e27a5e8d136f74ee46a9efe114f19be2f09fca
Ohene0909 <oheneanietey18@gmail.com>
2021-12-18 00:21:11 +0000

    adds Day  Arthimetic

---
+++ Day 2 Arthimetic.py
@@ -0,0 +1,23 @@
+a = int(input('Enter the fisrt value.. '))
+b = int(input('Enter the second value.. '))
+
+# addition
+print(f'the sum of {a} and {b} is {a+b}')
+
+# subtraction
+print(f'the difference of {a} and {b} is {a-b}')
+
+# multiplication
+print(f'the multiplication of {a} and {b} is {a*b}')
+
+# division
+print(f'the division of {a} and {b} is {a/b}')
+
+#modulus
+print(f'the remainder of {a} and {b} is {a%b}')
+
+# power
+print(f'the power of {a} over {b} is {a**b}')
+
+# int-div
+print(f'the int-quotient of {a} and {b} is {a//b}')
